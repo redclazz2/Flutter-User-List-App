8a71b5778fb9a072312f6ea629ec6aaa97e5b605
Sebastian Zarate <zaratesanmiguel2019@outlook.com>
2024-02-15 12:25:33 -0500

    Added for loop
    + Added a for loop on the widget creation.
    + Made the list an stateful widget due to future changes that might be made on the project.

--- lib/person_list_body.dart
+++ lib/person_list_body.dart
@@ -2,8 +2,21 @@ import 'package:flutter/material.dart';
 import 'package:flutter_application_1/person.dart';
 import 'package:flutter_application_1/person_widget.dart';
 
-class PersonListBody extends StatelessWidget {
+class PersonListBody extends StatefulWidget {
   const PersonListBody({super.key});
+
+  @override
+  State<PersonListBody> createState() => UserListState();
+}
+
+class UserListState extends State<PersonListBody> {
+  final List<Person> personPopulation = [
+    Person("firstName", "lastName", "studyField", 5, "images/prueba.jpg"),
+    Person("Sebastian", "Zarate", "Ing.Sistemas", 5, "images/prueba.jpg"),
+    Person("Sebastian", "Zarate", "Ing.Sistemas", 5, "images/prueba.jpg"),
+    Person("Fabian", "Gonzales", "Ing.Sistemas", 5, "images/prueba.jpg"),
+    Person("Fabian", "Gonzales", "Ing.Sistemas", 5, "images/prueba.jpg"),
+  ];
 
   @override
   Widget build(BuildContext context) {
@@ -14,9 +27,8 @@ class PersonListBody extends StatelessWidget {
         title: const Text("User List App - Sebastián Zárate"),
       ),
       body: Center(
-        child: ListView( children: [
-          PersonWidget(Person("Sebastian", "Zarate", "Ing. Sistemas", 5,"images/prueba.jpg")),
-          PersonWidget(Person("Sebastian", "Zarate", "Ing. Sistemas",4.56,"images/prueba.jpg")),
+        child: ListView(children: [
+          for (Person p in personPopulation) PersonWidget(p),
         ]),
       ),
     ));
